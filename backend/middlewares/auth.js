33f3a979b730f40383effda59c42fbb877e0d983
Theo Eschlimann <theo.eschlimann@gmail.com>
2020-10-06 18:56:31 +0200

    auth middlware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+
+module.exports = (req, res, next) => {
+    try {
+        //On prend le token de la requete
+        const token = req.headers.authorization.split(' ')[1];
+        //On le decode grace au TOKEN SECRET
+        const decodedToken = jwt.verify(token, 'SECRET_TOKEN');
+        //On extrait l'user id du TOKEN
+        const userId = decodedToken.userId;
+        //On le compare avec celui de la requete
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            next(); //Si il est juste on passe au middleware suivant
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request!')
+        });
+    }
+};
