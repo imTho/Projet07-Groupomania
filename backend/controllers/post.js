dea24b25233ffc4b492ed966fc896a69e07a7ceb
Theo Eschlimann <theo.eschlimann@gmail.com>
2020-10-07 23:27:09 +0200

    Post css and post authentification

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,7 +1,7 @@
 const db = require("../database_connect");
 
 exports.getAllPost = (req, res, next) => {
-    db.query('SELECT * FROM posts  ORDER BY date DESC', (error, result, field) => {
+    db.query('SELECT * FROM users INNER JOIN posts ON users.id = posts.userId ORDER BY date DESC', (error, result, field) => {
         if (error) {
             return res.status(400).json({
                 error
--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="posts">
-        Liste de posts
+        <div class="post" v-for = "post in posts" :key="post.id">
+            <div class="post-header">
+                <span class="post-info">Posté le : {{post.date}} par {{post.prenom}} {{post.nom}}</span> 
+                <span class="modify-btn" v-if = "user.userId == post.userId">Modifier le post</span> 
+            </div>  
+            <h2 class="post-title">{{post.title}}</h2>
+            <div class="post-content">{{post.content}}</div>
+        </div>
     </div>
 </template>
 
@@ -17,12 +24,13 @@ export default {
     },
 
     mounted() {
-        this.getAllPost()
+        this.user = JSON.parse(localStorage.user);
+        this.getAllPost();
     },
 
     methods: {
         getAllPost(){
-            let token = JSON.parse(localStorage.user).token;
+            const token = JSON.parse(localStorage.user).token;
 
             axios.get('http://localhost:3000/api/posts/getAllPost',
                 {
@@ -35,11 +43,43 @@ export default {
             .then(res => {
                 this.posts = res.data;
             })
-        }
+        },
     }
 }
 </script>
 
-<style>
+<style scoped>
+    .posts{
+        margin: 40px auto;
+        padding: 20px;
+        max-width: 800px;
+    }
+
+    .post{
+        position: relative;
+        padding: 20px;
+        margin-bottom: 30px;
+        border-radius: 20px;
+        box-shadow: 0px 0px 50px -7px rgba(0,0,0,0.2);
+        text-align: left;
+    }
+
+    .post h2{
+        margin-top: 7px;
+    }
+
+    .post-header{
+        display: flex;
+        justify-content: space-between;
+        color: rgb(175, 175, 175);
+        font-size: .8rem;
+    }
+
+    .modify-btn{
+        color: red;
+        cursor: pointer;
+    }
+
+    
 
 </style>
